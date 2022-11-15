5273750661edbc3916fce860d0061d3211e6d6db
Meg Grooms-Gangloff <102067932+meggrooms@users.noreply.github.com>
2022-11-14 20:41:20 -0800

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -23,10 +23,9 @@ cityData.forEach(function(city) {
 
 
 // We create the tile layer that will be the background of our map.
-// Dark Map code:
+// MAPBOX SYLES: https://docs.mapbox.com/api/maps/styles/#mapbox-styles
+
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// Light Map code:
-//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -45,3 +44,4 @@ cityData.forEach(function(city) {
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+Footer
