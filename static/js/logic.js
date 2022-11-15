a130c77889bed1033948e4decebd0a29dff82f3f
Meg Grooms-Gangloff <102067932+meggrooms@users.noreply.github.com>
2022-11-15 13:27:37 -0800

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,12 +1,9 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-//let map = L.map('mapid').setView([36.1733, -120.1794], 7);
-// Create the map object with center at the San Francisco airport.
+
 // Create the map object with center at the San Francisco airport.
 let map = L.map('mapid').setView([37.5, -122.5], 10);
-
 
 
 // Add GeoJSON data.
@@ -30,74 +27,27 @@ let sanFranAirport =
 ]};
 
 
+// Grabbing our GeoJSON data.
+// feature:GeoJSON feature, layer: properties of 
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer){
+    console.log(layer);
+    layer.bindPopup("<h2>" + "Airport Code: " + feature.properties.faa + "</h2>" + "<hr>" + "<h3>" + "Airport name: " + feature.properties.city + ", " + feature.properties.country + "</h3>")
 
+  }
 
-
-
-
-////////////single line uses just two sets
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [32.7338, -117.1933],
-  [37.6213, -122.3790],
-  [30.2672, -97.7431],
-  [43.6777, -79.6248],
-  [40.7128, -74.0060]
-];
-
-
-
-// Create a polyline using the line coordinates and make the line yellow.
-//dashed line: https://gis.stackexchange.com/questions/204121/how-to-set-dashed-line-on-leaflet
-L.polyline(line, {
-  color: "blue",
-  weight: '4',
-  dashArray: '5, 5', 
-  opacity: "0.5"
 }).addTo(map);
 
 
 
-// Get data from cities.js
-let cityData = cities;
 
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-      radius: city.population/200000,
-      color: "orange",
-      lineweight: 4
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
-
-
-
-// We create the tile layer that will be the background of our map.
-// Dark Map code // satellite layer
-  //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// Light Map code:
-  //https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToxken}
-//full color code:
-  //https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}'
-
-  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// Loop through the cities array and create one marker for each city.
-// // add comma to numbers: .toLocaleString()
-cityData.forEach(function(city) {
-  console.log(city)
-  L.marker(city.location)
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-.addTo(map);
-});
 
 
 
