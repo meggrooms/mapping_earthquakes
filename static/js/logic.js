47fb9c62dc859a603b4dd781c12c116b3834b054
Meg Grooms-Gangloff <102067932+meggrooms@users.noreply.github.com>
2022-11-14 20:47:25 -0800

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,61 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+//let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+// Create the map object with center at the San Francisco airport.
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+
+
+
+
+
+
+////////////single line uses just two sets
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [32.7338, -117.1933],
+  [37.6213, -122.3790],
+  [30.2672, -97.7431],
+  [43.6777, -79.6248],
+  [40.7128, -74.0060]
+];
+
+
+
+// Create a polyline using the line coordinates and make the line yellow.
+//dashed line: https://gis.stackexchange.com/questions/204121/how-to-set-dashed-line-on-leaflet
+L.polyline(line, {
+  color: "blue",
+  weight: '4',
+  dashArray: '5, 5', 
+  opacity: "0.5"
+}).addTo(map);
+
+
 
 // Get data from cities.js
 let cityData = cities;
@@ -23,10 +77,14 @@ cityData.forEach(function(city) {
 
 
 // We create the tile layer that will be the background of our map.
-// Dark Map code:
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Dark Map code // satellite layer
+  //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 // Light Map code:
-//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  //https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToxken}
+//full color code:
+  //https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}'
+
+  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
