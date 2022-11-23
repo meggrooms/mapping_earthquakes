ca815322c6f192d98bfc2298dffb291b684eb41b
Meg Grooms-Gangloff <102067932+meggrooms@users.noreply.github.com>
2022-11-23 12:35:24 -0800

    Add files via upload
    Display is not working despite the tutor and teachers helping me for several hours.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,45 +1,11 @@
+
+
 // Add console.log to check to see if our code is working.
 console.log("working");
 
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-
-// Grabbing our GeoJSON data.
-// feature:GeoJSON feature, layer: properties of 
-L.geoJSON(sanFranAirport, {
-  onEachFeature: function(feature, layer){
-    console.log(layer);
-    layer.bindPopup("<h2>" + "Airport Code: " + feature.properties.faa + "</h2>" + "<hr>" + "<h3>" + "Airport name: " + feature.properties.city + ", " + feature.properties.country + "</h3>")
-
-  }
-
-}).addTo(map);
-
-
-
+// Create the map object with center and zoom level.
+var map = L.map('mapid').setView([30, 30], 2);
 
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -49,7 +15,19 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-ni
 });
 
 
-
-
-// Then we add our 'graymap' tile layer to the map.
+//Then we add our 'streets' tile layer to the map
 streets.addTo(map);
+
+
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-online/module_13/majorAirports.json"; 
+
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+    console.log("done")
+});
