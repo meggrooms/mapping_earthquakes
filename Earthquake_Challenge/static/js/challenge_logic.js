4045136d4473942f2544c49f2bbd55ddf1e28772
Meg Grooms-Gangloff <102067932+meggrooms@users.noreply.github.com>
2022-12-03 15:01:54 -0800

    Add files via upload

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -42,14 +42,14 @@ let baseMaps = {
 // 1. Add a 3rd layer group for the major earthquake data.
 let allEarthquakes = new L.LayerGroup();
 let tetonicPlateData = new L.LayerGroup();
-let MajorEarthquakes = new L.LayerGroup();
+let majorEarthquakes = new L.LayerGroup();
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 // 2. Add a reference to the major earthquake group to the overlays object.
 let overlays = {
   "Earthquakes": allEarthquakes,
   "Tetonic Plate Data": tetonicPlateData,
-  "Major Earthquakes": MajorEarthquakes
+  "Major Earthquakes": majorEarthquakes
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -105,19 +105,23 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
+
     	// We turn each feature into a circleMarker on the map.
     	pointToLayer: function(feature, latlng) {
       		console.log(data);
       		return L.circleMarker(latlng);
         },
+
       // We set the style for each circleMarker using our styleInfo function.
     style: styleInfo,
+
      // We create a popup for each circleMarker to display the magnitude and location of the earthquake
      //  after the marker has been created and styled.
      onEachFeature: function(feature, layer) {
       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
     }
   }).addTo(allEarthquakes);
+
 
   // Then we add the earthquake layer to our map.
   allEarthquakes.addTo(map);
@@ -176,8 +180,8 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
     }
   // 8. Add the major earthquakes layer to the map.
-  }).addTo(MajorEarthquakes);
-  MajorEarthquakes.addTo(map)
+  }).addTo(majorEarthquakes);
+  majorEarthquakes.addTo(map)
   
   // 9. Close the braces and parentheses for the major earthquake data.
   });
@@ -232,4 +236,63 @@ legend.onAdd = function() {
   tetonicPlateData.addTo(map)
 });
 });
-Footer
+
+////// Deliverable 2 //////
+// 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
+
+  // 4. Use the same style as the earthquake data.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    if (magnitude > 6) {
+      return "#752234";
+    }
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    else {
+      return "#ea822c";
+    }
+    
+  }
+
+  // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
+  function  getRadius ( magnitude )  {
+    if (magnitude === 0) {
+      return  1 ;
+    }
+    return magnitude * 4;
+  }
+   
+  // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
+  // sets the style of the circle, and displays the magnitude and location of the earthquake
+  //  after the marker has been created and styled.
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  // 8. Add the major earthquakes layer to the map.
+  }).addTo(majorEarthquakes);
+  majorEarthquakes.addTo(map)
+  
+  // 9. Close the braces and parentheses for the major earthquake data.
+  });
+
+  
